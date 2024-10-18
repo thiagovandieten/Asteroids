819919a85d71e19c53921b37e68aaac324791d57
Thiago van Dieten <thiago@tvdieten.com>
2024-10-18 12:18:09 +0200

    Implement 60fps and delta variable

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while (True):
         for event in pygame.event.get():
@@ -16,6 +18,8 @@ def main():
 
         screen.fill(pygame.Color(0,0,0))
         pygame.display.flip()
+    
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
