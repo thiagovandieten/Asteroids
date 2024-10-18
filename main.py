8c10900c293c738120fb9c50eaca1d87e6eea7a8
Thiago van Dieten <thiago@tvdieten.com>
2024-10-18 23:51:55 +0200

    add player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting asteroids!")
@@ -17,6 +18,10 @@ def main():
                 return
 
         screen.fill(pygame.Color(0,0,0))
+
+        player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+        player.draw(screen)
         pygame.display.flip()
     
         dt = clock.tick(60) / 1000
