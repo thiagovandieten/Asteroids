1bf6a045cb342bb9abb379090f73cf5e5397132c
Thiago van Dieten <thiago@tvdieten.com>
2024-10-19 11:14:10 +0200

    use sprite groups

--- main.py
+++ main.py
@@ -8,20 +8,32 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+    
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    # updatable.add(player) # Not desired
+    # drawable.add(player) # Not desired
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while (True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        player.update(dt)
+        for obj in updatable:
+            obj.update(dt)
+        # player.update(dt)
 
         screen.fill(pygame.Color(0,0,0))
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
+        # player.draw(screen)
         
         pygame.display.flip()
     
